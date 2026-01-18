506b779670ab3825a928f909c11937e55ef7b508
Lucas Martins de Oliveira <118075030+LucasTMartins@users.noreply.github.com>
2026-01-18 13:41:29 -0300

    fixing edit github links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/lucastmartins/teste-doc',
+            'https://github.com/lucastmartins/teste-doc/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -61,7 +61,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/lucastmartins/teste-doc',
+            'https://github.com/lucastmartins/teste-doc/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
